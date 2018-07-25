bec9fef27e33e52581f0a3e04df9d1d6539bcf57
admin <>
2018-07-25 14:41:58 +0000

    {/src/main/java/com/myspace/drug_orders/Doctor.java}

--- src/main/java/com/myspace/drug_orders/Doctor.java
+++ src/main/java/com/myspace/drug_orders/Doctor.java
@@ -6,12 +6,47 @@ package com.myspace.drug_orders;
 
 public class Doctor implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Doctor() {
-    }
+	@org.kie.api.definition.type.Label(value = "Name")
+	private java.lang.String name;
+	@org.kie.api.definition.type.Label(value = "E-Mail")
+	private java.lang.String email;
+	@org.kie.api.definition.type.Label(value = "Mobile Number")
+	private java.lang.String mobileNo;
 
+	public Doctor() {
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
 
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public java.lang.String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(java.lang.String email) {
+		this.email = email;
+	}
+
+	public java.lang.String getMobileNo() {
+		return this.mobileNo;
+	}
+
+	public void setMobileNo(java.lang.String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public Doctor(java.lang.String name, java.lang.String email,
+			java.lang.String mobileNo) {
+		this.name = name;
+		this.email = email;
+		this.mobileNo = mobileNo;
+	}
 
 }
