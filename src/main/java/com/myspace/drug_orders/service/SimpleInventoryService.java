d32c4a53e99c6c89c63329e57dfe8a06b8f9aa13
Duncan Doyle <Duncan.Doyle@gmail.com>
2018-08-03 08:44:45 +0200

    Fixed SimpleInventoryService.

--- src/main/java/com/myspace/drug_orders/service/SimpleInventoryService.java
+++ src/main/java/com/myspace/drug_orders/service/SimpleInventoryService.java
@@ -1,5 +1,7 @@
 package com.myspace.drug_orders.service;
 
+import java.util.UUID;
+
 import com.myspace.drug_orders.model.Drug;
 import com.myspace.drug_orders.model.DrugStock;
 
@@ -7,7 +9,7 @@ public class SimpleInventoryService implements InventoryService {
 
 	public DrugStock getStockInfo(Drug drug) {
 		String skuNo = drug.getSkuNo();
-		DrugStock drugStock = new DrugStock(skuNo, Boolean.FALSE, Boolean.TRUE);
+		DrugStock drugStock = new DrugStock(skuNo, Boolean.FALSE, Boolean.TRUE, UUID.randomUUID().toString());
 		return drugStock;
 	}
 
